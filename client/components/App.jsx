ab01027e7ccba5a24f87f6eac19425cc6235d3bc
sarah-knowles <sjk339@gmail.com>
2020-11-22 12:41:51 +1300

    add final push

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -15,13 +15,24 @@ export class App extends React.Component {
     this.props.dispatch(fetchQuiz())
   }
 
+ answersArray (correct, incorrect) {
+   console.log(correct)
+   incorrect.push(correct)
+   console.log(incorrect)
+   return incorrect
+ }
+
   render () {
     return (
       <div className='app'>
         <h1>Answer Me!</h1>
         <ul> 
            {this.props.quiz.map((question, index) => (
-            <li key={index}>{question.question}  {question.correct_answer}</li>
+            <li key={index}>{question.question} 
+             {this.answersArray(question.correct_answer, question.incorrect_answers)
+             
+             }
+            </li>
           
           ))}
         </ul>
@@ -30,6 +41,8 @@ export class App extends React.Component {
   }
 }
 
+
+
 function mapStateToProps (globalState) {
   return {
     quiz: globalState.questions
@@ -37,3 +50,21 @@ function mapStateToProps (globalState) {
 }
 
 export default connect(mapStateToProps)(App)
+
+
+  //STRIPS HTML ENTITIES FROM CODE. CALL THIS WHEN YOU STRIP OUT DATA INTO VARIABLES LIKE I DID WITH THE QUESTION VARIABLE BELOW. HOW THIS ALL WORKS IS A MYSTERY TO ME AND IT IS SLIGHTLY HACKY. ESSENTIALLY USES THE BROWSER's DECODER TO DO THE WORK FOR YOU AND THEN CLAIMS THE RESULT BACK FOR YOUR NEFARIOUS PURPOSES
+
+//   decodeHTML(html) {
+//     var txt = document.createElement('textarea');
+//     txt.innerHTML = html;
+//     return txt.value;
+//   }
+
+//A LITTLE PART OF MY COMPONENT: RENDER WHERE THE MAGIC HAPPENS & RETURN THAT SHOWS IT
+
+// render() {
+//   let question = this.decodeHTML(this.props.question) 
+//       return(
+//         <h1>{question}</h1>
+//       )
+// }
